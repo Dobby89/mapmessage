8b117e4eed24d7c746d80986838abe6399143cdc
Robin <robin@jp74.com>
2014-07-10 16:02:04 +0100

    Added Disqus to the thread view

--- application/views/threads/view.php
+++ application/views/threads/view.php
@@ -3,16 +3,19 @@
 <h1><?php echo $thread['title']; ?></h1>
 <p><?php echo $thread['content']; ?></p>
 
-<?php if($comments) { ?>
+<div id="disqus_thread"></div>
+<script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'geogab'; // required: replace example with your forum shortname
 
-    <h2>Comments</h2>
-
-    <?php foreach($comments as $comment) { ?>
-
-        <div>
-            <p><?php echo $comment['content']; ?></p>
-        </div>
-    <?php } ?>
-<?php } ?>
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
 
 <?php $this->load->view('includes/footer'); ?>
